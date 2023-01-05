b25a5daf7df3f6340d3e243d533228d8ade5288d
Vetchems <113075046+Vetchems@users.noreply.github.com>
2023-01-05 01:38:40 +0000

    updated download code, auto retry and resume.

--- scripts/civitai-api.py
+++ scripts/civitai-api.py
@@ -9,30 +9,104 @@ import urllib.request
 import os
 from tqdm import tqdm
 import re
+from requests.exceptions import ConnectionError
+
 
 def download_file(url, file_name):
-    # Download the file and save it to a local file
-    #response = requests.get(url)
-    #open(file_name, "wb").write(response.content)
-    response = requests.get(url, stream=True)
+    # Maximum number of retries
+    max_retries = 5
 
-    # Get the total size of the file
-    total_size = int(response.headers.get("Content-Length", 0))
-    tokens = re.split(re.escape('\\'), file_name)
-    file_name_display = tokens[-1]
-    # Initialize the progress bar
-    progress = tqdm(total=total_size, unit="B", unit_scale=True, desc=f"Downloading {file_name_display}")
+    # Delay between retries (in seconds)
+    retry_delay = 10
 
-    # Open a local file to save the download
-    with open(file_name, "wb") as f:
-        # Iterate over the response chunks and update the progress bar
-        for chunk in response.iter_content(chunk_size=1024):
-            if chunk:  # filter out keep-alive new chunks
-                f.write(chunk)
-                progress.update(len(chunk))
+    while True:
+        # Check if the file has already been partially downloaded
+        if os.path.exists(file_name):
+            # Get the size of the downloaded file
+            downloaded_size = os.path.getsize(file_name)
 
-    # Close the progress bar
-    progress.close()
+            # Set the range of the request to start from the current size of the downloaded file
+            headers = {"Range": f"bytes={downloaded_size}-"}
+        else:
+            downloaded_size = 0
+            headers = {}
+
+        # Split filename from included path
+        tokens = re.split(re.escape('\\'), file_name)
+        file_name_display = tokens[-1]
+
+        # Initialize the progress bar
+        progress = tqdm(total=1000000000, unit="B", unit_scale=True, desc=f"Downloading {file_name_display}", initial=downloaded_size, leave=False)
+
+        # Open a local file to save the download
+        with open(file_name, "ab") as f:
+            while True:
+                try:
+                    # Send a GET request to the URL and save the response to the local file
+                    response = requests.get(url, headers=headers, stream=True)
+
+                    # Get the total size of the file
+                    total_size = int(response.headers.get("Content-Length", 0))
+
+                    # Update the total size of the progress bar if the `Content-Length` header is present
+                    if total_size == 0:
+                        total_size = downloaded_size
+                    progress.total = total_size 
+
+                    # Write the response to the local file and update the progress bar
+                    for chunk in response.iter_content(chunk_size=1024):
+                        if chunk:  # filter out keep-alive new chunks
+                            f.write(chunk)
+                            progress.update(len(chunk))
+
+                    downloaded_size = os.path.getsize(file_name)
+                    # Break out of the loop if the download is successful
+                    break
+                except ConnectionError as e:
+                    # Decrement the number of retries
+                    max_retries -= 1
+
+                    # If there are no more retries, raise the exception
+                    if max_retries == 0:
+                        raise e
+
+                    # Wait for the specified delay before retrying
+                    time.sleep(retry_delay)
+
+        # Close the progress bar
+        progress.close()
+        downloaded_size = os.path.getsize(file_name)
+        # Check if the download was successful
+        if downloaded_size >= total_size:
+            print(f"{file_name_display} successfully downloaded.")
+            break
+        else:
+            print(f"Error: File download failed. Retrying... {file_name_display}")
+
+#def download_file(url, file_name):
+#    # Download the file and save it to a local file
+#    response = requests.get(url, stream=True)
+#
+#    # Get the total size of the file
+#    total_size = int(response.headers.get("Content-Length", 0))
+#
+#    # Split filename from included path
+#    tokens = re.split(re.escape('\\'), file_name)
+#    file_name_display = tokens[-1]
+#
+#    # Initialize the progress bar
+#    progress = tqdm(total=total_size, unit="B", unit_scale=True, desc=f"Downloading {file_name_display}")
+#
+#    # Open a local file to save the download
+#    with open(file_name, "wb") as f:
+#        # Iterate over the response chunks and update the progress bar
+#        for chunk in response.iter_content(chunk_size=1024):
+#            if chunk:  # filter out keep-alive new chunks
+#                f.write(chunk)
+#                progress.update(len(chunk))
+#
+#    # Close the progress bar
+#    progress.close()
 
 def download_file_thread(url, file_name, content_type, use_new_folder, model_name):
     if content_type == "Checkpoint":
@@ -200,16 +274,11 @@ def update_dl_url(model_name=None, model_version=None, model_filename=None):
         model_version = model_version.replace(f' - {model_name}','').strip()
         for item in json_data['items']:
             if item['name'] == model_name:
-                print("found model name")
                 for model in item['modelVersions']:
                     if model['name'] == model_version:
-                        print("Found model version")
                         for file in model['files']:
                             if file['name'] == model_filename:
-                                print("Found model filename")
                                 dl_url = file['downloadUrl']
-                                print(f'{dl_url} - {file["name"]}')
-                            #dl_dict[file['name']] = file['downloadUrl']
         return gr.Textbox.update(value=dl_url)
     else:
         return gr.Textbox.update(value=None)
@@ -231,7 +300,7 @@ def update_model_info(model_name=None, model_version=None):
                 for model in item['modelVersions']:
                     if model['name'] == model_version:
                         if model['trainedWords']:
-                            output_training = " # ".join(model['trainedWords'])
+                            output_training = ", ".join(model['trainedWords'])
 
                         for file in model['files']:
                             dl_dict[file['name']] = file['downloadUrl']
@@ -284,7 +353,7 @@ def on_ui_tabs():
             list_versions = gr.Dropdown(label="Version", choices=[], interactive=True, elem_id="quicksettings", value=None)
         with gr.Row():
             txt_list = ""
-            dummy = gr.Textbox(label='Trained Tags (if any)', value=f'{txt_list}', interactive=False, lines=1)
+            dummy = gr.Textbox(label='Trained Tags (if any)', value=f'{txt_list}', interactive=True, lines=1)
             model_filename = gr.Dropdown(label="Model Filename", choices=[], interactive=True, value=None)
             dl_url = gr.Textbox(label="Download Url", interactive=False, value=None)
         with gr.Row():
