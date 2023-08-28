34bbced6bb91b1d90adef59f1a8b1699fc124ecc
BlafKing <9644716+BlafKing@users.noreply.github.com>
2023-08-28 04:21:06 +0200

    Update civitai-html.js to v1.5

--- javascript/civitai-html.js
+++ javascript/civitai-html.js
@@ -7,7 +7,15 @@ function select_model(model_name) {
 		let paddedNumber = String(randomNumber).padStart(3, '0');
 		model_dropdown.value = model_name + "." + paddedNumber;
 		updateInput(model_dropdown)
-
-		console.log("Selected Model:", model_name);
 	}
+}
+
+function updateCardSize(width, height) {
+    var styleSheet = document.styleSheets[0];
+    var imgKeyframes = `width: ${width}em !important; height: ${height}em !important;`;
+    var fontSize = (width / 8) * 100;
+    var textKeyframes = `font-size: ${fontSize}% !important;`;
+
+    styleSheet.insertRule(`.civmodelcard img { ${imgKeyframes} }`, styleSheet.cssRules.length);
+    styleSheet.insertRule(`.civmodelcard figcaption { ${textKeyframes} }`, styleSheet.cssRules.length);
 }
